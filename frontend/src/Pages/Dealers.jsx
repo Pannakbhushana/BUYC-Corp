9364168025dc0cdab84f80b0c66cb55b16210d1f
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2023-07-19 14:58:49 +0530

    dealers card added

--- frontend/src/Pages/Dealers.jsx
+++ frontend/src/Pages/Dealers.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import DealersCard from '../Components/DealersCard'
+import {Box,Grid} from '@chakra-ui/react';
 
 const Dealers = () => {
   return (
     <div>
-      <h1>Dealers page</h1>
+      <Box>
+        <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+            <DealersCard/>
+            <DealersCard/>
+            <DealersCard/>
+            <DealersCard/>
+        </Grid>
+      </Box>
+      
     </div>
   )
 }
